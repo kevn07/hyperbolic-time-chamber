739b880264a9d63a2c9309729c9b1358bcc7bf61
Kevin He <decade901@gmail.com>
2021-05-17 14:50:47 +1000

    added digio task

---
+++ exercises/digioTask/logProcess.test.js
@@ -0,0 +1,61 @@
+import {processLogByLine, getUniqueCount, getTopAcitivty, getTopNumber} from './logProcessor'
+
+
+const filePath = 
+describe('Testing Log Processor', () => {
+  it('return number of unique ip addresses', async () => {
+    const processedLog = await processLogByLine('./programming-task-example-data.log')
+    expect(getUniqueCount(processedLog.ipCount)).toBe(11)
+  })
+  it('return top 3 ip addresses', async () => {
+    const processedLog = await processLogByLine('./programming-task-example-data.log')
+    expect(getTopAcitivty(processedLog.ipCount, 3)).toStrictEqual([
+      { key: '168.41.191.40', count: 4 },
+      { key: '177.71.128.21', count: 3 },
+      { key: '50.112.00.11', count: 3 },
+      { key: '72.44.32.10', count: 3 },
+      { key: '168.41.191.9', count: 2 }
+    ])
+  })
+  it('return top 3 URLs', async () => {
+    const processedLog = await processLogByLine('./programming-task-example-data.log')
+    expect(getTopAcitivty(processedLog.urlCount, 3)).toStrictEqual([
+      { key: '/docs/manage-websites/', count: 2 },
+      { key: '/intranet-analytics/', count: 1 },
+      { key: 'http://example.net/faq/', count: 1 },
+      { key: '/this/page/does/not/exist/', count: 1 },
+      { key: 'http://example.net/blog/category/meta/', count: 1 },
+      { key: '/blog/2018/08/survey-your-opinion-matters/', count: 1 },
+      { key: '/docs/manage-users/', count: 1 },
+      { key: '/blog/category/community/', count: 1 },
+      { key: '/faq/', count: 1 },
+      { key: '/faq/how-to-install/', count: 1 },
+      { key: '/asset.js', count: 1 },
+      { key: '/to-an-error', count: 1 },
+      { key: '/', count: 1 },
+      { key: '/docs/', count: 1 },
+      { key: '/moved-permanently', count: 1 },
+      { key: '/temp-redirect', count: 1 },
+      { key: '/faq/how-to/', count: 1 },
+      { key: '/translations/', count: 1 },
+      { key: '/newsletter/', count: 1 },
+      { key: '/hosting/', count: 1 },
+      { key: '/download/counter/', count: 1 },
+      { key: '/asset.css', count: 1 }
+    ])
+  })
+  it('return first 3 urls & ip address', async () => {
+    const processedLog = await processLogByLine('./programming-task-example-data.log')
+    expect(getTopNumber(processedLog.urlCount, 3).length).toBe(3)
+    expect(getTopNumber(processedLog.ipCount, 3).length).toBe(3)
+  })
+  it('return empty list retrieving top 3 ip addresses from empty log', () => {
+    expect(getTopNumber({}, 3).length).toBe(0)
+  })
+  it('return 0 unique ip addresses from empty log', () => {
+    expect(getUniqueCount({}, 3)).toBe(0);
+  })
+  it('throw error retrieve top activity', () => {
+    expect(() => getTopAcitivty()).toThrow('getTopActivity: obj passed is invalid')
+  })
+})
---
+++ exercises/digioTask/logProcessor.js
@@ -0,0 +1,120 @@
+/**
+ * Code By: Kevin He
+ */
+import readline from 'readline';
+import fs from 'fs';
+
+/**
+ * processLog: function returns promise on resolve returns count of URL and IP addresses
+ * @param filePath
+ * @returns 
+ */
+export async function processLogByLine(filePath) {
+  try {
+    const fileStream = fs.createReadStream(filePath);
+    const readInterface = readline.createInterface({
+      input: fileStream
+    })
+    let ipCount = {};
+    let urlCount = {};
+    for await(const line of readInterface) {
+      const lineArray = line.split(' ')
+      if (!(ipCount[lineArray[0]])) {
+        let ipObj = {
+          count: 1
+        }
+        ipCount[lineArray[0]] = ipObj
+      } else {
+        ipCount[lineArray[0]].count += 1
+      }
+
+      // counts and pushes IP to result
+      if (!(urlCount[lineArray[6]])) {
+        let urlObj = {
+          count: 1
+        }
+        urlCount[lineArray[6]] = urlObj
+      } else {
+        urlCount[lineArray[6]].count += 1
+      }
+    }
+    return {
+      ipCount,
+      urlCount
+    }
+  } catch(err) {
+    console.error('processing log error: ', error)
+  }
+
+}
+
+/**
+ * Function returns length of unique keys given object
+ * @param obj 
+ * @returns 
+ */
+export function getUniqueCount(obj) {
+  return Object.keys(obj).length
+}
+
+/**
+ * Function returns top X number of param based on count of obj
+ * @param obj 
+ * @param top 
+ * @returns 
+ */
+export function getTopAcitivty(obj, top) {
+  if (!obj) {
+    throw new Error('getTopActivity: obj passed is invalid')
+  }
+  if (!(top > 0)) {
+    throw new Error('getTopActivity: top must be greater 0')
+  }
+
+  // sort object by count descending order
+  let sortedDesc = Object.keys(obj).sort((a, b) => {
+    return obj[b].count - obj[a].count
+  }).map(key => {
+    return {
+      key,
+      count: obj[key].count
+    }
+  })
+
+  if (top >= Object.keys(obj).length) {
+    return sortedDesc
+  }
+
+  // retrieve the top ranking objects if they are the same count then ranking is equal
+  let result = []
+  let currentTop = 1
+  let index = 0
+  let previous = -1
+  while (currentTop <= top && index < sortedDesc.length) {
+    result.push(sortedDesc[index])
+    if (previous !== sortedDesc[index].count) {
+      currentTop += 1
+      previous = sortedDesc[index].count
+    }
+    index += 1
+  }
+  return result
+}
+
+/**
+ * Function returns first X number of param based on count of obj
+ * @param obj 
+ * @param top 
+ * @returns 
+ */
+export function getTopNumber(obj, top) {
+  let sortedDesc = Object.keys(obj).sort((a, b) => {
+    return obj[b].count - obj[a].count
+  }).map(key => {
+    return {
+      key,
+      count: obj[key].count
+    }
+  })
+  return sortedDesc.slice(0, top)
+}
